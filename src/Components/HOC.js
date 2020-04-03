ac424253be11490c67a4b96bd294347dedfdb9ee
Tallons <Tallons27@yahoo.com>
2020-04-03 15:14:51 -0600

    practice

--- src/Components/HOC.js
+++ src/Components/HOC.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const DisplayList = () => {
+const withFilteredProps = (BaseComponent) => ({list, genre}) => {
+    const filteredList = genre ? list.filter(artist => artist.genre === genre) : list
+    return <BaseComponent artistList = {filteredList}/>
+} // withFilteredProps is returning another function - this is also destructure ({list, genre})
+
+const DisplayList = ({artistList}) => {
     return (
         <div>
-            HOC Component
+            {artistList.map((artist) => (
+                <div className='artist-container' key={artist.name}>
+                    <img className='artist-img' src={artist.artistImg} alt={artist.name} />
+                    <div className='artist-info'>
+                        <h3>{artist.name}</h3>
+                        <p>{artist.genre}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
 
-export default DisplayList;
+const HOC = withFilteredProps(DisplayList);
+
+export default HOC;
--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,30 +1,66 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect} from "react";
+import NameDisplay from "./NameDisplay";
 
-class Hooks extends Component {
-    constructor(){
-        super();
-        this.state = {
-            count: 0,
-            name: ''
-        }
-    }
+// import React, { Component } from 'react';
+const Hooks = () => {
+    const [count, setCount] = useState(0),
+          [name, setName] = useState(""),
+          [user, setUser] = useState({});
 
-    handleAdd(){
-    this.setState({ count: count + 1})
+    useEffect (() => {
+      /*  axios.get("request-url-here")
+            .then (res => {setUser(res.data)})
+            .catch(err => console.log(err)) */
+        console.log("useEffect = componentDidMount")
+    }, []); // without the [] dependency array. This array should contain what values you would like useEffect to 'watch'.
+
+    useEffect (() => {
+        console.log("useEffect = componentDidUpdate")
+    }, [count, name]);
+// class Hooks extends Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             count: 0,
+//             name: ''
+//         }
+//     }
+
+    // const handleAdd = () => {   HOOK EXAMPLE
+    //     setCount(count + 1);
+    // }
+
+//     handleAdd(){
+//     this.setState({ count: count + 1})
+// }
+
+    // const handleInput = (event) => {   HOOK EXAMPLE
+    //     setName(event.target.value);
+    // }
+
+//     handleInput(value){
+//      this.setState({name: e.target.value})
+// }
+    return (
+        <div>
+            <input value={name} onChange={(event) => setName(event.target.value)}/>
+            {/* <input value={name} onChange={(event) => handleInput(event)}/> */}
+            <NameDisplay name={name}/>
+            {count}
+            {/* <button onClick={handleAdd}>+</button> */}
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
+        </div>
+    )
 }
-
-    handleInput(value){
-     this.setState({name: e.target.value})
-}
-
-    render(){
-        return(
-            <div>
-                {this.state.count}
-                 <input onChange={(e) => this.handleInput(e.target.value)}/>
-            </div>
-        )
-    }
-}
+//     render(){
+//         return(
+//             <div>
+//                 {this.state.count}
+//                  <input onChange={(e) => this.handleInput(e.target.value)}/>
+//             </div>
+//         )
+//     }
+// }
 
 export default Hooks;
